7d85d0017825ef2941c58e7d34b9affe5cfe4d4c
printo3d <grawires@gmail.com>
2020-07-09 00:51:20 +0200

    revertttt

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -81,26 +81,6 @@
 //#define PRINTO_H3_PLUS
 //#define PRINTO_H3_TOWER
 //#define PRINTO_H3_TOWERPLUS
-//#define PRINTO_H3_MIDI
-//#define PRINTO_H3_MIDI350
-//#define PRINTO_H3_BIGGIE
-
-
-
-#if defined(PRINTO_H3_PLUS) || defined(PRINTO_H3_TOWERPLUS) || defined(PRINTO_H3_BIGGIE)
-	#define PLOSS_SUPPORT
-	#define PLOSS_MANUAL_RECOVERY
-  #define NEXTION_AUTO_BED_LEVEL     // bilinear bed leveling - auto
-#endif
-#if defined(PRINTO_H3) || defined(PRINTO_H3_TOWER)
-	#define NEXTION_SEMIAUTO_BED_LEVEL  // mesh bed leveling - semi auto
-	//#define PLOSS_SUPPORT
-	//#define PLOSS_MANUAL_RECOVERY
-#endif
-
-// BED LEVELING NEXTION
-	//#define NEXTION_SEMIAUTO_BED_LEVEL  // mesh bed leveling - semi auto
-  //#define NEXTION_AUTO_BED_LEVEL     // bilinear bed leveling - auto
 
 
 /**
@@ -1263,22 +1243,7 @@
 //#define AUTO_BED_LEVELING_LINEAR
 //#define AUTO_BED_LEVELING_BILINEAR
 //#define AUTO_BED_LEVELING_UBL
-//#define MESH_BED_LEVELING
-
-// NEXTION BED DEF
-  // semi auto
-  #ifdef NEXTION_SEMIAUTO_BED_LEVEL
-    #define MESH_BED_LEVELING
-    #define PROBE_MANUALLY 
-    #define LCD_Z_STEP 0.04
-    #define LCD_PROBE_Z_RANGE 16
-  #endif
-
-  // auto
-  #ifdef NEXTION_AUTO_BED_LEVEL 
-    #define AUTO_BED_LEVELING_BILINEAR
-    #define FIX_MOUNTED_PROBE  
-  #endif
+#define MESH_BED_LEVELING
 
 /**
  * Normally G28 leaves leveling disabled on completion. Enable
@@ -2395,6 +2360,12 @@
 	  #define NEXTION_JERK_SETTINGS
   #endif
   //#define NEXTION_STEP_SETTINGS
+
+// BED LEVELING NEXTION
+	#define NEXTION_BED_LEVEL
+	#define LCD_Z_STEP 0.04
+  #define LCD_PROBE_Z_RANGE 16
+	#define PROBE_MANUALLY
 
 // FSENSOR ONOFF
 	//#define FSENSOR_STATE
