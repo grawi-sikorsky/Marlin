be6e533a64d198b8bdeba81929a0e06bfda93f13
Printo3DJob <grawires@gmail.com>
2020-10-26 17:21:20 +0100

    predkosci mniejsze

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -679,7 +679,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 800, 160.0 }     // LV8729  (1/16, 1/16, 1/32, 1/16)
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 800, 60.0 }     // LV8729  (1/16, 1/16, 1/32, 1/16)
 //#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160, 160, 800, 180 }// LV8729  (1/32, 1/32, 1/32, 1/32)
 //#define DEFAULT_AXIS_STEPS_PER_UNIT   { 320, 320, 800, 90 } // LV8729 (1/64, 1/64, 1/32, 1/16)
 //#define DEFAULT_AXIS_STEPS_PER_UNIT   { 640, 640, 800, 90 } // LV8729 (1/128, 1/128, 1/32, 1/16)}
@@ -689,7 +689,7 @@
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 260, 260, 45, 80 }
+#define DEFAULT_MAX_FEEDRATE          { 200, 200, 35, 80 }
 
 /**
  * Default Max Acceleration (change/s) change = mm/s
@@ -702,7 +702,7 @@
 #if ENABLED (NEXTION_AUTO_BED_LEVEL)
   #define DEFAULT_MAX_ACCELERATION      { 5000, 5000, 15, 9000 }
 #else
-  #define DEFAULT_MAX_ACCELERATION      { 5000, 5000, 30, 9000 }
+  #define DEFAULT_MAX_ACCELERATION      { 1800, 1800, 25, 9000 }
 #endif
 /**
  * Default Acceleration (change/s) change = mm/s
@@ -712,9 +712,9 @@
  *   M204 R    Retract Acceleration
  *   M204 T    Travel Acceleration
  */
-#define DEFAULT_ACCELERATION          1200    // X, Y, Z and E acceleration for printing moves
-#define DEFAULT_RETRACT_ACCELERATION  3000    // E acceleration for retracts
-#define DEFAULT_TRAVEL_ACCELERATION   1200    // X, Y, Z acceleration for travel (non printing) moves
+#define DEFAULT_ACCELERATION          1200     // X, Y, Z and E acceleration for printing moves
+#define DEFAULT_RETRACT_ACCELERATION  2000    // E acceleration for retracts
+#define DEFAULT_TRAVEL_ACCELERATION   1200     // X, Y, Z acceleration for travel (non printing) moves
 
 /**
  * Default Jerk (mm/s)
@@ -724,9 +724,9 @@
  * When changing speed and direction, if the difference is less than the
  * value set here, it may happen instantaneously.
  */
-#define DEFAULT_XJERK                 20.0
-#define DEFAULT_YJERK                 20.0
-#define DEFAULT_ZJERK                  0.4
+#define DEFAULT_XJERK                 10.0
+#define DEFAULT_YJERK                 10.0
+#define DEFAULT_ZJERK                  0.2
 #define DEFAULT_EJERK                  5.0
 
 #if ENABLED (NEXTION_AUTO_BED_LEVEL)
@@ -1287,12 +1287,12 @@
 #endif
 
 // Homing speeds (mm/m)
-#define HOMING_FEEDRATE_XY (125*60)
+#define HOMING_FEEDRATE_XY (110*60)
 
 #if ENABLED (NEXTION_AUTO_BED_LEVEL)
   #define HOMING_FEEDRATE_Z  (15*60)
 #else
-  #define HOMING_FEEDRATE_Z  (45*60)
+  #define HOMING_FEEDRATE_Z  (30*60)
 #endif
 
 
