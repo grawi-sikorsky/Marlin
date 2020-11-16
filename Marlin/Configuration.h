a3cab30ea2c1726557f4c64b8ccf381379a7ff8f
Printo3DJob <grawires@gmail.com>
2020-11-16 17:03:38 +0100

    PLOSS OFF default - jebie karty sd chybajednakk...

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -82,8 +82,8 @@
 //#define PRINTO_H3_PLUS
 //#define PRINTO_H3_TOWER
 //#define PRINTO_H3_TOWERPLUS
-//#define PRINTO_H3_MIDI
-#define PRINTO_H3_MIDI350
+#define PRINTO_H3_MIDI
+//#define PRINTO_H3_MIDI350
 //#define PRINTO_H3_BIGGIE
 
 // Wybor sterownika:
